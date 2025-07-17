c1f72737b176085ad48887405306452efab9e1f3
vendingcompute <edutre1999@gmail.com>
2025-07-16 19:07:50 -0500

    x

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
   return (
     <section
       id="hero"
-      className="w-full flex flex-col justify-center items-center px-2 pt-28 md:pt-20 bg-gradient-to-r from-emerald-600 to-blue-600 min-h-[70vh] md:h-screen"
+      className="w-full flex flex-col justify-center items-center px-2 py-16 bg-gradient-to-r from-emerald-600 to-blue-600 min-h-[70vh] md:h-screen"
     >
       <div className="w-full border-6 border-gray-200 p-4 flex flex-col items-center relative">
         <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-8 text-white leading-relaxed text-left w-full px-4 py-4 md:px-16 md:py-10 lg:px-32 lg:py-16">
@@ -38,7 +38,7 @@ export default function HeroSection() {
               <span className="text-sm">edutrecompute@gmail.com</span>
             </a>
             <a
-              href="tel:+19562345678"
+              href="tel:+19562619316"
               className="flex items-center gap-2 text-white hover:text-emerald-200 transition-colors"
             >
               <Phone className="w-4 h-4" />
